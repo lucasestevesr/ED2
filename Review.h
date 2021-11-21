a42328457b363ceabba3d9ae6c554a58c1ff744d
rafafhorta <rafaelafhorta@gmail.com>
2021-11-21 20:03:10 -0300

    Ajustes na formatação add

--- Review.h
+++ Review.h
@@ -6,30 +6,43 @@
 using namespace std;
 
 class Review {
-    private:
-        string id;
-        string text;
-        int upvotes;
-        string app_version;
-        string posted_date;
+private:
+    string id;
+    string text;
+    int upvotes;
+    string app_version;
+    string posted_date;
 
-    public:
-        Review(string id, string text, int upvotes, string app_version, string posted_date);
-        Review();
-        ~Review();
-        string getId();
-        void setId(string id);
-        string getText();
-        void setText(string text);
-        int getUpvotes();
-        void setUpvotes(int upvotes);
-        string getAppVersion();
-        void setAppVersion(string app_version);
-        string getPostedDate();
-        void setPostedDate(string posted_date);
-        void imprimir();
+public:
+    Review(string id, string text, int upvotes, string app_version, string posted_date);
 
-        static Review* recuperarReviewPeloId(ifstream &arquivo_processado, int id);
+    Review();
+
+    ~Review();
+
+    string getId();
+
+    void setId(string id);
+
+    string getText();
+
+    void setText(string text);
+
+    int getUpvotes();
+
+    void setUpvotes(int upvotes);
+
+    string getAppVersion();
+
+    void setAppVersion(string app_version);
+
+    string getPostedDate();
+
+    void setPostedDate(string posted_date);
+
+    void imprimir();
+
+    static Review *recuperarReviewPeloId(ifstream &arquivo_processado, int id);
 };
 
 #endif //ED2_REVIEW_H
--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ void selecionar(int selecao, ifstream &arquivo_processado) {
             int id;
             cin >> id;
             Review *review = Review::recuperarReviewPeloId(arquivo_processado, id);
-            if(review != nullptr) {
+            if (review != nullptr) {
                 review->imprimir();
             } else {
                 cout << "Erro: Registro não encontrado!" << endl;
@@ -42,7 +42,7 @@ void selecionar(int selecao, ifstream &arquivo_processado) {
             cout << "[1]-Console      [2]-Arquivo de Texto" << endl;
             int opcao;
             cin >> opcao;
-            if(opcao == 1) {
+            if (opcao == 1) {
                 cout << "Importar para console com N = 10" << endl;
             } else {
                 cout << "Importar para texto com N = 100" << endl;
@@ -57,32 +57,31 @@ void selecionar(int selecao, ifstream &arquivo_processado) {
 
 void mainMenu(ifstream &arquivo_processado) {
     int selecao = 1;
-    while(selecao != 0)
-    {
+    while (selecao != 0) {
         selecao = menu();
         selecionar(selecao, arquivo_processado);
     }
 }
 
-bool buscarColunas(string linha, string* colunas, bool &entreAspas, int &colunaAtual) {
+bool buscarColunas(string linha, string *colunas, bool &entreAspas, int &colunaAtual) {
     string dado = "";
-    for(int i = 0; i < linha.size(); i++) {
-        if(linha[i] == '"' && !entreAspas) {
+    for (int i = 0; i < linha.size(); i++) {
+        if (linha[i] == '"' && !entreAspas) {
             entreAspas = true;
-        }else if(linha[i] == '"' && entreAspas) {
+        } else if (linha[i] == '"' && entreAspas) {
             entreAspas = false;
         }
-        if(linha[i] == ',' && !entreAspas) {
+        if (linha[i] == ',' && !entreAspas) {
             colunas[colunaAtual] += dado;
             dado = "";
             colunaAtual++;
-        }else {
+        } else {
             dado += linha[i];
         }
-        if(colunaAtual == 4) {
+        if (colunaAtual == 4) {
             dado = "";
-            for(int j = linha.size(); j > 0; j--) {
-                if(linha[j] == ',') {
+            for (int j = linha.size(); j > 0; j--) {
+                if (linha[j] == ',') {
                     colunas[colunaAtual] += dado;
                     return true;
                 }
@@ -90,7 +89,7 @@ bool buscarColunas(string linha, string* colunas, bool &entreAspas, int &colunaA
             }
             break;
         }
-        if(i == linha.size() - 1) {
+        if (i == linha.size() - 1) {
             colunas[colunaAtual] += dado;
             break;
         }
@@ -100,7 +99,7 @@ bool buscarColunas(string linha, string* colunas, bool &entreAspas, int &colunaA
 
 void salvarString(ofstream &arquivo_bin, string valor) {
     size_t tamanho = valor.size();
-    arquivo_bin.write((char*)&tamanho, sizeof(tamanho));
+    arquivo_bin.write((char *) &tamanho, sizeof(tamanho));
 //  arquivo_bin.write(reinterpret_cast<char*>(&tamanho), sizeof(size_t));
     arquivo_bin.write(valor.c_str(), tamanho);
 }
@@ -110,7 +109,7 @@ void processar(ifstream &arquivo_csv, ofstream &arquivo_bin) {
     int qnt_linhas = 0;
     string linha = "";
     int colunaAtual;
-    string* colunas;
+    string *colunas;
     bool entreAspas;
     bool resposta;
     int upvotesAux;
@@ -118,31 +117,31 @@ void processar(ifstream &arquivo_csv, ofstream &arquivo_bin) {
     getline(arquivo_csv, linha, '\n');
 
     while (getline(arquivo_csv, linha, '\n')) {
-        if(linha.size() > 0) {
+        if (linha.size() > 0) {
             colunas = new string[5];
             entreAspas = false;
             colunaAtual = 0;
 
             resposta = buscarColunas(linha, colunas, entreAspas, colunaAtual);
 
-            if(!resposta) {
+            if (!resposta) {
                 do {
                     getline(arquivo_csv, linha, '\n');
                     resposta = buscarColunas(linha, colunas, entreAspas, colunaAtual);
-                }while(!resposta);
+                } while (!resposta);
             }
             Review *review = new Review(colunas[0], colunas[1], stoi(colunas[2]), colunas[3], colunas[4]);
 //            review->imprimir();
             salvarString(arquivo_bin, review->getId());
             salvarString(arquivo_bin, review->getText());
             upvotesAux = review->getUpvotes();
-            arquivo_bin.write((char*)&upvotesAux, sizeof(int));
+            arquivo_bin.write((char *) &upvotesAux, sizeof(int));
 //            arquivo_bin.write(reinterpret_cast<char*>(&upvotesAux), sizeof(int));
             salvarString(arquivo_bin, review->getAppVersion());
             salvarString(arquivo_bin, review->getPostedDate());
 
             qnt_linhas++;
-            delete [] colunas;
+            delete[] colunas;
         }
     }
     arquivo_bin.close();
@@ -159,14 +158,14 @@ int main(int argc, char const *argv[]) {
     ifstream arquivo_bin;
     arquivo_bin.open(argv[1] + nome_bin, ios::binary);
 
-    if(arquivo_bin.is_open()) {
+    if (arquivo_bin.is_open()) {
         mainMenu(arquivo_bin);
     } else {
         arquivo_bin.close();
         ifstream arquivo_csv;
         arquivo_csv.open(argv[1] + nome_csv);
 
-        if(arquivo_csv.is_open()) {
+        if (arquivo_csv.is_open()) {
             ofstream arquivo_bin;
             arquivo_bin.open(argv[1] + nome_bin, ios::binary | ios::trunc);
 
@@ -177,7 +176,7 @@ int main(int argc, char const *argv[]) {
 
             ifstream arquivo_processado;
             arquivo_processado.open(argv[1] + nome_bin, ios::binary);
-            if(arquivo_processado.is_open()) {
+            if (arquivo_processado.is_open()) {
                 mainMenu(arquivo_processado);
             } else {
                 cout << "Erro: Nao foi possivel abrir o arquivo bin '" << nome_bin << "'" << endl;
