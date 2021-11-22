4dcd793197e079f317d06ee08709d8edff75214f
Pedro Paulo <pedropaulosilvaf@gmail.com>
2021-11-22 19:20:26 -0300

    comentando códigos

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@
 
 using namespace std;
 
+// Definindo constantes para os nomes dos arquivos
 const string nome_csv = "tiktok_app_reviews.csv";
 const string nome_bin = "tiktok_app_reviews.bin";
 const string nome_txt = "export_reviews.txt";
 
+// Inicio função menu de opções
 int menu() {
     int selecao;
     cout << "-------------------- MENU --------------------" << endl;
@@ -17,69 +19,99 @@ int menu() {
     cout << "[2] testeImportacao():" << endl;
     cout << "[0] Sair" << endl;
     cin >> selecao;
+    // Retorna a opção escolhida pelo usuário
     return selecao;
 }
+// Fim função menu de opções
 
+// Início função selecionar
 void selecionar(int selecao, ifstream &arquivo_processado, string diretorio) {
+    // Função serve para fazer o switch da opção escolhida pelo usuário
     switch (selecao) {
         case 0: {
+            // Caso escolha 0, fecha o arquivo e finaliza o programa
             cout << "Programa finalizado!" << endl;
             arquivo_processado.close();
             exit(0);
             break;
         }
         case 1: {
+            // Recupera quantidade total de reviews para exibir a faixa para o usuário
             int total = Review::recuperarQuantidadeReviews(arquivo_processado);
+            // Pergunta e le qual id de review ele quer acessar
             cout << "O arquivo contem " << total << " reviews, digite um indice:"  << endl;
             int id;
             cin >> id;
+            // Starta o cronometro
             auto start = std::chrono::high_resolution_clock::now();
+            // Chama a função para recuperar o Review pelo id
             Review *review = Review::recuperarReviewPeloId(arquivo_processado, id);
+            // Se o review retornado for diferente de nullptr vai imprimi-lo
             if (review != nullptr) {
                 review->imprimir();
             } else {
+                // Se não encontrar o review exibe um erro
                 cout << "Erro: Review nao encontrado!" << endl;
             }
+            // Finaliza cronometro e exibe o tempo em milissegundos
             auto end = std::chrono::high_resolution_clock::now();
             auto int_m = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
             cout << "O tempo de busca do Id=" << id << " foi de " << to_string(int_m.count()) << " milissegundos." << endl;
             break;
         }
         case 2: {
+            // Exibe as opções para o usuário e le
             cout << "[1]-Console      [2]-Arquivo de Texto" << endl;
             int opcao;
             cin >> opcao;
+            // Seta a seed do aleatório
             srand(time(0));
+            // Recupera a quantidade de reviews salva dentro do arquivo binário
             int total = Review::recuperarQuantidadeReviews(arquivo_processado);
+            // Se a opção do usuário for 1 é pra exibir n=10 reviews no console
             if (opcao == 1) {
+                // Starta cronometro
                 auto start = std::chrono::high_resolution_clock::now();
+                // Declara quantidade de Reviews
                 int n = 10;
                 int intAleatorio;
+                // Loop de 0 a n-1 para pegar N reviews aleatórios
                 for(int i = 0; i < n; i++) {
+                    // Pega o int aleatório
                     intAleatorio = rand()%(total) + 1;
+                    // Escreve seu id
                     cout << "Review ID = " << intAleatorio << " abaixo:" << endl;
+                    // Recupera o Review
                     Review *review = Review::recuperarReviewPeloId(arquivo_processado, intAleatorio);
+                    // Se for diferente de null, escreve o Review, se for null escreve erro
                     if (review != nullptr) {
                         review->imprimir();
                     } else {
                         cout << "Erro: Review nao encontrado!" << endl;
                     }
                 }
+                // Finaliza cronometro e exibe o tempo em milissegundos
                 auto end = std::chrono::high_resolution_clock::now();
                 auto int_m = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
                 cout << "O tempo para exibir " << n << " reviews aleatorios no console foi de " << to_string(int_m.count()) << " milissegundos." << endl;
             } else if(opcao == 2) {
+                // Starta cronometro
                 auto start = std::chrono::high_resolution_clock::now();
                 int n = 100;
                 cout << "Exportando " << n << " reviews para o arquivo " << nome_txt << endl;
+                // Abre o arquivo txt para exportar os reviews
                 ofstream arquivo_txt;
                 arquivo_txt.open(diretorio + nome_txt, ios::trunc);
+                // Escreve um cabeçalho com a quantidade de reviews exportados no arquivo txt
                 arquivo_txt << "Contêm " << n << " reviews neste arquivo." << endl << endl;
                 int intAleatorio;
+                // Loop para pegar N registros aleatórios
                 for(int i = 0; i < n; i++) {
+                    // Pega um int aleatório e busca seu Review correspondente
                     intAleatorio = rand()%(total) + 1;
                     Review *review = Review::recuperarReviewPeloId(arquivo_processado, intAleatorio);
                     if (review != nullptr) {
+                        // Escreve o review no arquivo txt
                         arquivo_txt << "Index: " << intAleatorio << endl;
                         arquivo_txt << "Id: " << review->getId() << endl;
                         arquivo_txt << "Text: " << review->getText() << endl;
@@ -87,10 +119,13 @@ void selecionar(int selecao, ifstream &arquivo_processado, string diretorio) {
                         arquivo_txt << "App Version: " << review->getAppVersion() << endl;
                         arquivo_txt << "Posted Date: " << review->getPostedDate() << endl << endl;
                     } else {
+                        // Se não encontrar da erro
                         cout << "Erro: Review " << intAleatorio << " nao encontrado!" << endl;
                     }
                 }
+                // Fecha o arquivo txt
                 arquivo_txt.close();
+                // Finaliza cronometro e exibe o tempo em milissegundos
                 auto end = std::chrono::high_resolution_clock::now();
                 auto int_m = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
                 cout << "O tempo para exportar " << n << " reviews aleatorios foi de " << to_string(int_m.count()) << " milissegundos." << endl;
@@ -105,6 +140,7 @@ void selecionar(int selecao, ifstream &arquivo_processado, string diretorio) {
         }
     }
 }
+// Fim função selecionar
 
 void mainMenu(ifstream &arquivo_processado, string diretorio) {
     int selecao = 1;
