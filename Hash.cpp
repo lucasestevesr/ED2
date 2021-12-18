8796212a9d6044721432abb91ea4db381dc2f0be
Pedro Paulo <pedropaulosilvaf@gmail.com>
2021-12-18 16:06:17 -0300

    básico da classe hash falta fazer função de formula e inserir

--- Hash.cpp
+++ Hash.cpp
@@ -1,1 +1,32 @@
 #include "Hash.h"
+
+Hash::Hash(int tamanho) {
+    this->tamanho = tamanho;
+    this->hashEncadeado = new HashEncadeadoPonteiro [tamanho];
+    for(int i = 0; i < tamanho; i++) {
+        this->hashEncadeado[i] = new HashEncadeado();
+    }
+}
+
+Hash::~Hash() {
+    for(int i = 0; i < tamanho; i++) {
+        delete this->hashEncadeado[i];
+    }
+    delete [] this->hashEncadeado;
+}
+
+int Hash::getTamanho() {
+    return this->tamanho;
+}
+
+void Hash::setTamanho(int tamanho) {
+    this->tamanho = tamanho;
+}
+
+HashEncadeadoPonteiro* Hash::getHashEncadeado() {
+    return this->hashEncadeado;
+}
+
+void Hash::setHashEncadeado(HashEncadeadoPonteiro *hashEncadeado) {
+    this->hashEncadeado = hashEncadeado;
+}
--- Hash.h
+++ Hash.h
@@ -1,8 +1,21 @@
 #ifndef ED2_HASH_H
 #define ED2_HASH_H
 
-class Hash {
+#include "HashEncadeado.h"
 
+typedef HashEncadeado* HashEncadeadoPonteiro;
+
+class Hash {
+    private:
+        int tamanho;
+        HashEncadeadoPonteiro *hashEncadeado;
+    public:
+        Hash(int tamanho);
+        ~Hash();
+        int getTamanho();
+        void setTamanho(int tamanho);
+        HashEncadeadoPonteiro* getHashEncadeado();
+        void setHashEncadeado(HashEncadeadoPonteiro *hashEncadeado);
 };
 
 #endif //ED2_HASH_H
